d511685db0bb9616c39366b529ba710a8f9e9fc4
oscarjoker10 <sibayoscar@hotmail.com>
2017-08-25 13:56:31 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -9,6 +9,10 @@ var randomQ;
 var rquestionT;
 var rquestionTc;
 var shuffleC;
+var iscorrect=false;
+var actualans="";
+var isright;
+var points=0;
 function setup()
 {
     var height = window.innerHeight*.97;
@@ -18,28 +22,70 @@ function setup()
     rquestion = questions[randomQ];
     rquestionTc=(int)(random(0, 2));
     rquestionT = questionType[rquestionTc];
+  
+    if (rquestionTc == 0) {
+        rchoice.push(Canswers[randomQ]);
+        actualans=Canswers[randomQ];
+    }
+    if (rquestionTc == 1) {
+        rchoice.push(Sanswers[randomQ]);
+        actualans=Sanswers[randomQ]
+    }
+    for(var t =0; t<3; t++)
+        {
+            
+            rchoice.push(Canswers[(int)(random(0, Canswers.length-1))]);
+
+        }
+    
+   // rchoice.push(Canswers[(int)(random(0, Canswers.length - 1))]);
+   // rchoice.push(Canswers[(int)(random(0, Canswers.length - 1))]);
+  
+ 
+     shuffleC = shuffle(rchoice);
+}
+function reset()
+{ 
+    points=points+10;
+    iscorrect=false;
+    randomQ=(int)(random(0, questions.length - 1));
+    rquestion = questions[randomQ];
+    for(var h =0; h<=3;h++)
+        {
+            rchoice.splice(h);
+        }
+    rquestionTc=(int)(random(0, 2));
+    rquestionT = questionType[rquestionTc];
     rchoice.push(Canswers[(int)(random(0, Canswers.length-1))]);
     rchoice.push(Canswers[(int)(random(0, Canswers.length - 1))]);
     rchoice.push(Canswers[(int)(random(0, Canswers.length - 1))]);
     if (rquestionTc == 0) {
         rchoice.push(Canswers[randomQ]);
+        actualans=Canswers[randomQ];
     }
     if (rquestionTc == 1) {
         rchoice.push(Sanswers[randomQ]);
+        actualans=Sanswers[randomQ];
     }
      shuffleC = shuffle(rchoice);
 }
+
 function draw()
 {
    
     background(0);
     Question();
+   // background(244, 248, 252);
+   // line(mouseX, 0, mouseX, 100);
+   // line(0, mouseY, 100, mouseY);
     answers();
 }
 function Question()
 {
     textSize(100);
     text(rquestionT+" "+rquestion, (width/2)-170, height*.25);
+  textSize(50);
+    text("SCORE:: "+points,20,60);
     fill(0, 102, 153, 51);
     
   
@@ -48,20 +94,67 @@ function Question()
 function answers()
 {
    
-    
-    choices.push(new answer(.20, .30, shuffleC[0]));
-    choices.push(new answer(.60, .30, shuffleC[1]));
-    choices.push(new answer(.20, .60, shuffleC[2]));
-    choices.push(new answer(.60, .60, shuffleC[3]));
+    if(shuffleC[0]==(actualans))
+        {
+    choices.push(new answer(.20, .30, shuffleC[0],true));
+        }
+        if(shuffleC[0]!=(actualans))
+            {
+        choices.push(new answer(.20, .30, shuffleC[0],false));
+            }
+
+        if(shuffleC[1]==(actualans))
+            {
+    choices.push(new answer(.60, .30, shuffleC[1],true));
+    }
+    if(shuffleC[1]!=(actualans))
+        {
+choices.push(new answer(.60, .30, shuffleC[1],false));
+}
+
+        if(shuffleC[2]==(actualans))
+            {
+    choices.push(new answer(.20, .60, shuffleC[2],true));
+    }
+    if(shuffleC[2]!=(actualans))
+        {
+choices.push(new answer(.20, .60, shuffleC[2],false));
+}
+        if(shuffleC[3]==(actualans))
+            {
+    choices.push(new answer(.60, .60, shuffleC[3],true));
+            }
+            if(shuffleC[3]!=(actualans))
+                {
+        choices.push(new answer(.60, .60, shuffleC[3],false));
+                }
    
 
 }
-function answer(x,y,lol)
+function answer(x,y,lol,correct)
 {
+    textSize(width*.08);
     fill(0, 0, 1000);
+    if(mouseX>=width*x && mouseX<=(width*.20)+width*x)
+        {
+            if(mouseY>=height*y && mouseY<=(height*.20)+height*y )
+                {
+                    if(mouseIsPressed)
+                        {
+                            if(correct==true)
+                                {
+                    fill(0,200,0);
+                   
+                    reset();
+                                }
+
+                                 
+                        }
+                }
+        }
     rect(width * x, height * y, width * .20, height * .20);
-  
+    
     fill(225);
-   text(lol, (width * x)+40, (height * y)+120);
+   text(lol, (width * x), (height * y)+height*(.20));
    
 }
